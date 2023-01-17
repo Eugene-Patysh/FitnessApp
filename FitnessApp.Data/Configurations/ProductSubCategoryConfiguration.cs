e8c45205d84bfc87a711e7fc570800f2869b8227
Pablo120590 <mr_pablo@mail.ru>
2023-01-17 11:55:29 +0300

    #6: Fixed Data.Configurations

--- FitnessApp.Data/Configurations/ProductSubCategoryConfiguration.cs
+++ FitnessApp.Data/Configurations/ProductSubCategoryConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using FitnessApp.Data.Models;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FitnessApp.Data.Configurations
 {
@@ -16,7 +11,7 @@ namespace FitnessApp.Data.Configurations
             builder.ToTable("ProductSubCategories").HasKey(t => t.Id); // configure table name and set primary key
             builder.Property(_ => _.Id).ValueGeneratedOnAdd(); // auto creating id when entity is added
             builder.Property(_ => _.Title).IsRequired().HasMaxLength(30); // field to require for fill. Set numbers of max symbols
-            builder.HasOne(_ => _.ProductCategory).WithMany(_ => _.ProductSubCategories).HasForeignKey(_ => _.ProductCategoryId);
+            builder.HasOne(_ => _.ProductCategory).WithMany(_ => _.ProductSubCategories).HasForeignKey(_ => _.ProductCategoryId).OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
