ae11536186062155e5e72df32afe7eb6409eb1c0
Pablo120590 <mr_pablo@mail.ru>
2023-01-17 12:29:32 +0300

    #6: Fixed Logic.Models

--- FitnessApp.Logic/Models/NutrientCategoryDto.cs
+++ FitnessApp.Logic/Models/NutrientCategoryDto.cs
@@ -1,11 +1,4 @@
-﻿using FitnessApp.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FitnessApp.Logic.Models
+﻿namespace FitnessApp.Logic.Models
 {
     public class NutrientCategoryDto
     {
